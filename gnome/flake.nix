b24ca55ac23e8413f55c80d6957f3b29c2b23093
Peter Bittner <peter@painless.software>
2025-10-31 20:30:41 +0100

    Add GNOME application concept using Python

---
+++ gnome/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "GNOME development shell for Nix";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    mkDevShell = system: {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
+        # shellHook = "exec /run/current-system/sw/bin/zsh";
+        buildInputs = with nixpkgs.legacyPackages.${system}; [
+          gtk3
+        ] ++ (with python313Packages; [
+          darkdetect
+          pycairo
+          pygobject3
+        ]);
+      };
+    };
+  in {
+    devShells = nixpkgs.lib.genAttrs supportedSystems (system: mkDevShell system);
+  };
+}
